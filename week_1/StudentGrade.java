4a093b123b47334152f39d22ad85284c993ebab6
Am1neK <aminekis16@gmail.com>
2026-08-16 06:21:26 +0000

    Complete Week 1 tutorial

---
+++ week_1/StudentGrade.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class StudentGrade {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter student name: ");
+        String name = input.nextLine();
+
+        System.out.print("Enter mark: ");
+        double mark = input.nextDouble();
+
+        String grade;
+
+        if (mark >= 80) {
+            grade = "A";
+        } else if (mark >= 70) {
+            grade = "B";
+        } else if (mark >= 60) {
+            grade = "C";
+        } else if (mark >= 50) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+
+        System.out.println("\n===== STUDENT RESULT =====");
+        System.out.println("Student Name : " + name);
+        System.out.println("Mark         : " + mark);
+        System.out.println("Grade        : " + grade);
+
+        input.close();
+    }
+}
